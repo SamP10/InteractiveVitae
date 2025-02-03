c6a90555a2cb0055b3914cf8b0c765c854b2c305
SamP10 <sam.plant998@gmail.com>
2025-02-03 23:05:52 +0000

    refactor for moving states

--- src/app/components/worldContent.tsx
+++ src/app/components/worldContent.tsx
@@ -1,16 +1,38 @@
 'use client';
 
 import { useRef, useEffect, useState } from 'react';
-import { Engine, Render, World, Events } from 'matter-js';
+import { Engine, Render, World } from 'matter-js';
 import StartButton from './startButton';
+import Introduction from './introduction';
+
+enum PAGE_STATE {
+  START = 'START',
+  INTRO = 'INTRO',
+  QUALIFICATIONS = 'QUALIFICATIONS',
+  PROJECTS = 'PROJECTS'
+}
 
 export default function WorldContent() {
   const scene = useRef(null);
   const engine = useRef(Engine.create());
-  const [bodies, setBodies] = useState([]);
-  const {innerHeight, innerWidth} = window;
+  const bodiesRef = useRef([]);
+  const [currentPage, setCurrentPage] = useState(PAGE_STATE.START);
+  const [radius, setRadius] = useState(0);
+  let timesCalled = 0;
+
+
+  const [{ innerHeight, innerWidth }, setInnerWidthHeight] = useState({
+    innerWidth: 0,
+    innerHeight: 0
+  });
 
   useEffect(() => {
+    setInnerWidthHeight({ innerWidth: window.innerWidth, innerHeight: window.innerHeight });
+  }, []);
+
+  useEffect(() => {
+    if (!innerWidth || !innerHeight) return;
+
     const render = Render.create({
       element: scene.current,
       engine: engine.current,
@@ -18,14 +40,15 @@ export default function WorldContent() {
         width: innerWidth,
         height: innerHeight,
         wireframes: false,
-        background: 'transparent',
-      },
+        background: 'transparent'
+      }
     });
 
     Render.run(render);
 
     const runner = () => {
       Engine.update(engine.current, 16);
+      cleanupOutOfBoundBodies();
       requestAnimationFrame(runner);
     };
 
@@ -37,31 +60,43 @@ export default function WorldContent() {
       Engine.clear(engine.current);
       render.canvas.remove();
     };
-  }, []);
+  }, [innerHeight, innerWidth]);
 
   const addBody = (body) => {
-    setBodies((existingBodies) => [...existingBodies, body]);
+    bodiesRef.current.push(body);
     World.add(engine.current.world, [body]);
-  }
-
-  const cleanupOutOfBoundsBodies = () => {
-    setBodies((prevBodies) =>
-      prevBodies.filter((body) => {
-        if (body.position.y > innerHeight) {
-          World.remove(engine.current.world, body);
-          return false;
-        }
-        return true;
-      })
-    );
   };
 
-  Events.on(engine.current, 'afterUpdate', cleanupOutOfBoundsBodies);
+  const cleanupOutOfBoundBodies = () => {
+    bodiesRef.current = bodiesRef.current.filter((body) => {
+      if (body.position.y > innerHeight) {
+        World.remove(engine.current.world, body);
+        movePageState();
+        return false;
+      }
+      return true;
+    });
+  };
+
+  const movePageState = () => {
+    setCurrentPage((previousPageState) => {
+      const pageStates = Object.values(PAGE_STATE);
+      const currentIndex = pageStates.indexOf(previousPageState);
+
+      if (currentIndex < pageStates.length - 1) {
+        return pageStates[currentIndex + 1];
+      }
+      return previousPageState;
+    });
+  };
 
   return (
     <div className="relative w-full h-screen">
       <div ref={scene} className="absolute inset-0 z-0" />
-      <StartButton onAddBody={addBody}/>
+      {currentPage === PAGE_STATE.START && <StartButton onAddBody={addBody} onSetRadius={setRadius} />}
+      {currentPage === PAGE_STATE.INTRO && <Introduction onAddBody={addBody} radius={radius} />}
+      {currentPage === PAGE_STATE.QUALIFICATIONS && <p>QU</p>}
+      {currentPage === PAGE_STATE.PROJECTS && <p>Projects Content</p>}
     </div>
   );
 }
