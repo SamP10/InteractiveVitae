fdf1ec1f0475af112f3fe7ae73d690ca230465c8
SamP10 <sam.plant998@gmail.com>
2025-02-03 23:00:30 +0000

    make radius consistent

--- src/app/components/startButton.tsx
+++ src/app/components/startButton.tsx
@@ -3,9 +3,9 @@
 import { useState, useRef } from 'react';
 import { Bodies } from 'matter-js';
 
-export default function StartButton({ onAddBody }) {
+export default function StartButton({ onAddBody, onSetRadius }) {
   const [buttonClicked, setButtonClicked] = useState(false);
-  const [showCircle, setShowCircle] = useState(false);
+  const [dropBall, setDropBall] = useState(false);
   const buttonRef = useRef(null);
 
   const handleClick = () => {
@@ -16,6 +16,7 @@ export default function StartButton({ onAddBody }) {
       const cx = rect.left + rect.width / 2;
       const cy = rect.top + rect.height / 2;
       const radius = rect.width / 2;
+      onSetRadius(radius);
 
       const circle = Bodies.circle(cx, cy, radius, {
         restitution: 0.9,
@@ -25,7 +26,7 @@ export default function StartButton({ onAddBody }) {
         }
       });
 
-      setShowCircle(true);
+      setDropBall(true);
 
       onAddBody(circle);
     }, 300);
@@ -33,7 +34,7 @@ export default function StartButton({ onAddBody }) {
 
   return (
     <div className="flex items-center justify-center w-full h-screen">
-      {!showCircle && (
+      {!dropBall && (
         <button
           ref={buttonRef}
           className={`bg-yellow-500 hover:bg-yellow-300 text-white font-bold rounded-full transform transition-all duration-300 ${
