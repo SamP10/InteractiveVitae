e31c0b490e4c74d9ecbaf5e2c1c6a6dfce1059a0
SamP10 <sam.plant998@gmail.com>
2025-02-05 00:41:45 +0000

    Do some wild pipe work

---
+++ src/app/components/prefabs/pipes.tsx
@@ -0,0 +1,256 @@
+import { Bodies } from 'matter-js';
+import { ANGLES } from './angleConstants';
+import { Secular_One } from 'next/font/google';
+
+export function verticalPipe({
+  x = 0,
+  y = 0,
+  offset = 0,
+  width = 500,
+  height = 700,
+  color = '#fbbf24'
+}): Bodies[] {
+  return [
+    Bodies.rectangle(x, y, width, height, {
+      isStatic: true,
+      render: {
+        fillStyle: color
+      }
+    }),
+    Bodies.rectangle(x + offset, y, width, height, {
+      isStatic: true,
+      render: {
+        fillStyle: color
+      }
+    })
+  ];
+}
+
+export function horizontalPipe({
+  x = 0,
+  y = 0,
+  offset = 0,
+  width = 500,
+  height = 700,
+  color = '#fbbf24'
+}): Bodies[] {
+  return [
+    Bodies.rectangle(x, y, width, height, {
+      isStatic: true,
+      render: {
+        fillStyle: color
+      }
+    }),
+    Bodies.rectangle(x, y + offset, width, height, {
+      isStatic: true,
+      render: {
+        fillStyle: color
+      }
+    })
+  ];
+}
+
+export function fullPipeCurve() {
+  const pipeCurves = [];
+  const radius = 110;
+  const innerRadius = 10;
+  let x = 110;
+  let y = 0;
+  pipeCurves.push(...quarterPipe({ x, y, startAngle: ANGLES[90], radius, innerRadius }));
+  pipeCurves.push(
+    ...uBendPipe({
+      x,
+      y: (y += radius + innerRadius),
+      angles: [ANGLES[270], ANGLES[360]],
+      radius,
+      innerRadius
+    })
+  );
+  pipeCurves.push(
+    ...uBendPipe({
+      x,
+      y: (y += radius + innerRadius),
+      angles: [ANGLES[180], ANGLES[90]],
+      radius,
+      innerRadius
+    })
+  );
+  pipeCurves.push(
+    ...uBendPipe({
+      x,
+      y: (y += radius + innerRadius),
+      angles: [ANGLES[270], ANGLES[360]],
+      radius,
+      innerRadius
+    })
+  );
+  pipeCurves.push(
+    ...uBendPipe({
+      x,
+      y: (y += radius + innerRadius),
+      angles: [ANGLES[180], ANGLES[90]],
+      radius,
+      innerRadius
+    })
+  );
+  pipeCurves.push(
+    ...sBendPipe({
+      x,
+      y: (y += radius + innerRadius),
+      angles: [ANGLES[270], ANGLES[90]],
+      radius,
+      innerRadius
+    })
+  );
+  pipeCurves.push(
+    ...sBendPipe({
+    x: (x+= radius + innerRadius),
+      y: (y += radius + innerRadius),
+      angles: [ANGLES[270], ANGLES[90]],
+      radius,
+      innerRadius
+    })
+  );
+
+  return pipeCurves;
+}
+
+export function sBendPipe({
+  x = 0,
+  y = 0,
+  angles = [ANGLES[180], ANGLES[270]],
+  arcAngle = ANGLES[90],
+  radius = 110,
+  innerRadius = 15,
+  outerNumPoints = 20,
+  innerNumPoints = 5,
+  color = '#fbbf24'
+}) {
+  let secondAngleX = x;
+
+  let secondAngleY = y;
+
+  if (angles[0] === ANGLES[180] && angles[1] === ANGLES[360]) {
+    secondAngleY -= radius + innerRadius;
+  } else if (angles[0] === ANGLES[360] && angles[1] === ANGLES[180]) {
+    secondAngleX += radius + innerRadius;
+  } else if (angles[0] === ANGLES[90] && angles[1] === ANGLES[270]) {
+    secondAngleY -= radius + innerRadius;
+  } else if (angles[0] === ANGLES[270] && angles[1] === ANGLES[90]) {
+    secondAngleX += radius + innerRadius;
+  }
+  return [
+    ...quarterPipe({
+      x,
+      y,
+      arcAngle,
+      startAngle: angles[0],
+      radius,
+      innerRadius,
+      outerNumPoints,
+      innerNumPoints,
+      color
+    }),
+    ...quarterPipe({
+      x: secondAngleX,
+      y: secondAngleY,
+      arcAngle,
+      startAngle: angles[1],
+      radius,
+      innerRadius,
+      outerNumPoints,
+      innerNumPoints,
+      color
+    })
+  ];
+}
+
+export function uBendPipe({
+  x = 0,
+  y = 0,
+  angles = [ANGLES[180], ANGLES[270]],
+  arcAngle = ANGLES[90],
+  radius = 110,
+  innerRadius = 15,
+  outerNumPoints = 20,
+  innerNumPoints = 5,
+  color = '#fbbf24'
+}) {
+  return [
+    ...quarterPipe({
+      x,
+      y,
+      arcAngle,
+      startAngle: angles[0],
+      radius,
+      innerRadius,
+      outerNumPoints,
+      innerNumPoints,
+      color
+    }),
+    ...quarterPipe({
+      x,
+      y,
+      arcAngle,
+      startAngle: angles[1],
+      radius,
+      innerRadius,
+      outerNumPoints,
+      innerNumPoints,
+      color
+    })
+  ];
+}
+
+export function quarterPipe({
+  x = 0,
+  y = 0,
+  startAngle = ANGLES[180],
+  arcAngle = ANGLES[90],
+  radius = 110,
+  innerRadius = 15,
+  outerNumPoints = 15,
+  innerNumPoints = 3,
+  color = '#fbbf24'
+}) {
+  return [
+    // Outer arc
+    ...circleArc({ x, y, startAngle, arcAngle, radius, numPoints: outerNumPoints, color }),
+    // Inner arc
+    ...circleArc({
+      x,
+      y,
+      startAngle,
+      arcAngle,
+      radius: innerRadius,
+      numPoints: innerNumPoints,
+      color
+    })
+  ];
+}
+
+function circleArc({
+  x = 0,
+  y = 0,
+  startAngle = ANGLES[180],
+  arcAngle = ANGLES[90],
+  radius = 110,
+  numPoints = 60,
+  color = '#fbbf24'
+}): Bodies[] {
+  const curveBodies = [];
+
+  for (let i = 0; i < numPoints; i++) {
+    const angle = startAngle + arcAngle * (i / (numPoints - 1));
+    const localX = x + radius * Math.cos(angle);
+    const localY = y + radius * Math.sin(angle);
+
+    const peg = Bodies.circle(localX, localY, 2.5, {
+      isStatic: true,
+      render: { fillStyle: color }
+    });
+    curveBodies.push(peg);
+  }
+
+  return curveBodies;
+}
