3eb59534d3bada521d6d67302c3a174e4f41a0e2
SamP10 <sam.plant998@gmail.com>
2025-02-15 23:47:28 +0000

    feat: add wiggly straight pipe

---
+++ src/app/components/prefab-pipes/wigglyStraightPipe.tsx
@@ -0,0 +1,95 @@
+import { Bodies, Composite } from 'matter-js';
+import { ANGLES } from './angleConstants';
+import AbstractPipe from './abstractPipe';
+import PipeFactory from '../composites/pipeFactory';
+
+class WigglyStraightPipe extends AbstractPipe {
+    constructor(
+        positionX: number,
+        positionY: number,
+        radius: number,
+        onAddBodies: (bodiesToAdd: Bodies[] | Composite[]) => void
+    ) {
+        super(positionX, positionY, radius, onAddBodies);
+        this.createBalls(2500);
+    }
+    createPipe(): void {
+        const innerRadius = 10;
+        let x = this.positionX - this.radius;
+        let y = this.positionY;
+        const pipeColor = this.pipeColors[Math.floor(Math.random() * this.pipeColors.length)];
+
+        PipeFactory.quarterPipe({
+            x,
+            y: this.positionY,
+            startAngle: ANGLES[360],
+            radius: this.diameter + 15,
+            innerRadius,
+            composite: this.composite,
+            color: pipeColor
+        });
+        PipeFactory.sBendPipe({
+            x: (x -= this.diameter + 15 + innerRadius),
+            y: (y += this.diameter + 15 + innerRadius),
+            angles: [ANGLES[360], ANGLES[180]],
+            radius: this.diameter + 15,
+            innerRadius,
+            composite: this.composite,
+            color: pipeColor
+        });
+        PipeFactory.sBendPipe({
+            x: (x -= this.diameter + 15 + innerRadius),
+            y: (y += this.diameter + 15 + innerRadius),
+            angles: [ANGLES[360], ANGLES[180]],
+            radius: this.diameter + 15,
+            innerRadius,
+            composite: this.composite,
+            color: pipeColor
+        });
+        PipeFactory.quarterPipe({
+            x,
+            y: (y += this.diameter + 15 + innerRadius),
+            startAngle: ANGLES[180],
+            radius: this.diameter + 15,
+            innerRadius,
+            composite: this.composite,
+            color: pipeColor
+        });
+        PipeFactory.vertical({
+            x: (x -= this.diameter + 15 + innerRadius),
+            y,
+            offset: this.diameter + 15,
+            numSegments: 30,
+            segmentLength: 10,
+            composite: this.composite,
+            color: pipeColor
+        });
+        y = this.composite.bodies[this.composite.bodies.length - 1].position.y;
+        PipeFactory.quarterPipe({
+            x,
+            y,
+            startAngle: ANGLES[360],
+            radius: this.diameter + 15,
+            innerRadius,
+            composite: this.composite,
+            color: pipeColor
+        });
+        PipeFactory.horizontalLeft({
+            x,
+            y,
+            offset: this.diameter + 15,
+            numSegments: 110,
+            segmentLength: 10,
+            composite: this.composite,
+            color: pipeColor
+        });
+
+        this.composite.bodies.forEach((body) => {
+            body.collisionFilter.group = this.collisionGroup;
+            body.collisionFilter.mask = this.collisionMask;
+        });
+        this.onAddBodies([this.composite]);
+    }
+}
+
+export default WigglyStraightPipe;
