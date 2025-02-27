85f441993566354fe0eb752168590b1502b4f86d
SamP10 <sam.plant998@gmail.com>
2025-02-27 20:57:55 +0000

    feat: calculate random positions on the screen

--- src/app/components/introduction/introduction.tsx
+++ src/app/components/introduction/introduction.tsx
@@ -21,70 +21,24 @@ export default function Introduction({
 
     useEffect(() => {
         if (!addedPipes.current) {
-            new CurvyDownPipe(50, -20, radius, onAddBodies);
-            new CurvyHorizontalPipe(200, -20, radius, onAddBodies);
-            new SuperWigglyPipe(500, -20, radius - 10, onAddBodies);
-            new WigglyStraightPipe(1300, -10, radius + 5, onAddBodies);
+            new CurvyDownPipe(Math.random() * width, -20, radius, onAddBodies, { windowX: width, windowY: height });
+            new CurvyHorizontalPipe(Math.random() * width/2, -20, radius, onAddBodies, {
+                windowX: width,
+                windowY: height
+            });
+            new SuperWigglyPipe(Math.random() * width, -20, radius - 10, onAddBodies, {
+                windowX: width,
+                windowY: height
+            });
+            new WigglyStraightPipe(Math.random() * width * 0.25 + width * 0.75, -10, radius + 5, onAddBodies, {
+                windowX: width,
+                windowY: height
+            });
             addedPipes.current = true;
         }
 
-        // const floor = Bodies.rectangle(width / 2, height, width, 10, {
-        //     isStatic: true,
-        //     render: { fillStyle: '#EAB308' }
-        // });
-
-        // const car = Car({
-        //     x: width / 4,
-        //     y: height - 30,
-        //     width: radius * 2,
-        //     height: radius / 4,
-        //     wheelSize: radius / 4,
-        //     wheelBase: radius * 2
-        // });
-
-        // onAddBodies([floor]);
-
         return () => {};
     }, [engine, onAddBodies, radius, width, height]);
-
-    // const handleCollision = (event) => {
-    //     const pairs = event.pairs;
-    //     for (let i = 0; i < pairs.length; i++) {
-    //         const {bodyA, bodyB} = pairs[i];
-
-    //         if (
-    //             (bodyA === ballRef.current && bodyB === floorRef.current) ||
-    //             (bodyA === floorRef.current && bodyB === ballRef.current)
-    //         ) {
-    //             const targetPoint = { x: 300, y: 0 };
-    //             const dx = targetPoint.x - ballRef.current.position.x;
-    //             const dy = targetPoint.y - ballRef.current.position.y;
-    //             const distance = Math.sqrt(dx * dx + dy * dy);
-
-    //             const forceMagnitude = 0.05;
-    //             const force = {
-    //                 x: (dx / distance) * forceMagnitude,
-    //                 y: 0
-    //             };
-
-    //             Body.applyForce(ballRef.current, ballRef.current.position, force);
-    //         }
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     Events.on(engine.current, 'collisionStart', handleCollision);
-
-    //     return () => {
-    //         Events.off(engine.current, 'collisionStart', handleCollision);
-    //     };
-    // }, [engine]);
-
-    // const addPipes = (radius: number) => {
-    //     const diameter = radius * 2;
-
-    //     onAddBodies([getPipes(diameter)]);
-    // };
 
     return <div className="relative w-full h-screen"></div>;
 }
