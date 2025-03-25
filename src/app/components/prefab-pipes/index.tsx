ff1cf042994a56d2d11a9ce9af3b4fdb3bfca383
SamP10 <sam.plant998@gmail.com>
2025-03-25 00:53:32 +0000

    feat: export horizontal

--- src/app/components/prefab-pipes/index.tsx
+++ src/app/components/prefab-pipes/index.tsx
@@ -2,5 +2,6 @@ import CurvyDownPipe from './curvyDownPipe';
 import CurvyHorizontalPipe from './curvyHorizontalPipe';
 import SuperWigglyPipe from './superWigglyPipe';
 import WigglyStraightPipe from './wigglyStraightPipe';
+import Horizontal1 from './horizontal1';
 
-export { CurvyDownPipe, CurvyHorizontalPipe, SuperWigglyPipe, WigglyStraightPipe };
+export { CurvyDownPipe, CurvyHorizontalPipe, SuperWigglyPipe, WigglyStraightPipe, Horizontal1 };
