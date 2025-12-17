56c40c437dff0c9347584547a403a2bf740ca7ee
SamP10 <sam.plant998@gmail.com>
2025-12-17 16:17:37 +0000

    UPDATE animation to be better

--- src/app/components/navigation/navigationBar/navigationBar.tsx
+++ src/app/components/navigation/navigationBar/navigationBar.tsx
@@ -6,7 +6,7 @@ export default function NavigationBar() {
 
     // timing should start after the calling card icons finish (last icon starts at 1.4s and runs 1.5s => ~2.9s)
     const baseDelay = 3.0; // seconds
-    const stagger = 0.12; // seconds between each item
+    const stagger = 0.25; // seconds between each item
 
     return (
         <div className="inline-flex border-b-2 border-cream pb-2 w-full">
@@ -14,15 +14,16 @@ export default function NavigationBar() {
             <div className="flex-1 flex items-center justify-center m-auto gap-24 relative">
                 {navItems.map((label, i) => {
                     const underlineDelay = baseDelay + i * stagger;
-                    const textDelay = underlineDelay + 0.18; // text pops up after underline
+                    const textDelay = underlineDelay + 0.3; // text pops up after underline
 
                     return (
                         <span key={label} className="inline-flex flex-col items-center relative">
-                            <span
+                            <a
                                 className="text-cream text-sm md:text-md lg:text-lg animate-nav-pop-up"
-                                style={{ animationDelay: `${textDelay}s` }}>
+                                href="#"
+                                style={{ animationDelay: `${textDelay}s`}}>
                                 {label}
-                            </span>
+                            </a>
                             <div
                                 className="nav-underline animate-nav-underline"
                                 style={{ animationDelay: `${underlineDelay}s` }}
